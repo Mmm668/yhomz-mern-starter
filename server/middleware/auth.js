058d9b35708d90bcb770e1e4f48efd43428ef5f8
BY <devblossom77@gmail.com>
2020-10-20 13:36:21 +0900

    [server] auth 미들웨어 세팅

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,20 @@
-const { User } = require('../models/User');
+const {User} = require('../models/User');
 
-let auth = (req, res, next) => {
-    let token = req.cookies.w_auth;
+let auth = (req, res, next) => { // next도 기본 props 인가?
+    let token = req.cookies.w_auth; // 1.cookie에 토큰 받아오는 구조?  2.w_auth?
 
-    User.findByToken(token, (err, user) => {
+    User.findByToken(token, (err, user)=>{
+        if(err) throw err;
+        if(!user)
+            return res.json({
+                isAuth: false,
+                error: true
+            });
 
+        req.token = token;
+        req.user = user;
+        next();
     })
 }
+
+module.exports = { auth };
