3ddc7ecb50aae87157929bce5f1840016cfc1d87
BY <devblossom77@gmail.com>
2020-10-20 15:33:15 +0900

    [api] product - /getProducts 추가 (list 정렬 메소드 사용)

--- server/routes/product.js
+++ server/routes/product.js
@@ -45,4 +45,56 @@ router.post('/uploadProduct', auth, (req, res) => {
         return res.status(200).json({success: true})
     })
 })
+
+// [model].find 와 [model].findOneAndUpdate 쓰임새, 차이점
+// find안에의 $in, populate()
+// result list 정렬 
+router.post('/getProducts', (req, res) => {
+    let order = req.body.order ? req.body.order : 'desc';
+    let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+
+    let findArgs = {};
+    let term = req.body.searchTerm; // true / false ?
+
+    for (let key in req.body.filters) { // filters는 어떤 형태지, 무튼 findArgs 포매팅
+        if (req.body.filters[key].length > 0) {
+            if (key === 'price') {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                }
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    console.log('@@ findArgs');
+
+    if (term) {
+        Product.find(findArgs)
+            .find({$text: {$search: term}})
+            .populate('writer')
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({success: false, err})
+                res.status(200).json({success: true, products, postSize: products.length})
+            })
+    } else {
+        Product.find(findArgs)
+            .populate('writer')
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({success: false, err})
+                res.status(200).json({success: true, products, postSize: products.length})
+            })
+    }
+})
+
 module.exports = router;
