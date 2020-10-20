5873986af639df446ac4f8ef833d0bc3c9c1048f
BY <devblossom77@gmail.com>
2020-10-20 13:35:25 +0900

    [server] express 세팅

--- server/index.js
+++ server/index.js
@@ -2,23 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = requier('cors');
-
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
 const config = require('./config/key');
 
-const {User} = require('./models/User')
-const port = 5000;
-
-// req의 application/x-www-form-urlencoded 포멧의 코드를 분석(parse)해주는 역할
-// 한마디로 아래 엔드포인트 함수에서 req.body 가능하게 해주
-app.use(bodyParser.urlencoded({extended: true}))
-// application/json 형태 분석(parse)해주는 역할
-app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('',
+mongoose
+    .connect('',
     {
         useNewUrlParser: true, useUnifiedTopology: true,
         useCreateIndex: true, useFindAndModify: false
@@ -26,17 +17,28 @@ mongoose.connect('',
     .then(() => console.log('@@ mongoDB connected..'))
     .catch(err => console.log('@@ err', err));
 
-app.get('/', (req, res) => res.send('hello world!'));
+app.use(cors())
 
-app.post('/register', (req, res) => {
-    // 회원 가입 필요한 정보들 client에서 가져오면
-    // 그것들을 db에 넣어준다
+app.use(bodyParser.urlencoded({extended: true})) // req의 application/x-www-form-urlencoded 포멧의 코드를 분석(parse)해주는 역할 = 아래 엔드포인트 함수에서 req.body 가능하게 해줌
+app.use(bodyParser.json()); // application/json 형태 분석(parse)해주는 역할
+app.use(cookieParser());
 
-    const user = new User(req.body);
-    user.save((err, userInfo) => {
-        if (err) return res.json({success: false, err})
-        return res.status(200).json({success: true})
+app.use('./api/users', require('./routes/users'));
+app.use('./api/product', require('./routes/product'));
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+    // index.html for all page routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
     });
-})
+}
 
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`running on ${port}`));
